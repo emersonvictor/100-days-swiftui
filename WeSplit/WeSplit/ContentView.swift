a5658ccc73fd4118cd1ef6a99d31b56045063a55
Emerson Victor <emersonvvictor@gmail.com>
2020-02-17 10:45:47 -0300

    Day 17 and 18

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Form data
+    @State private var orderSubtotal = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 2
+    let tipPercentages = [0,10, 15, 20, 25]
+    
+    // MARK: - Computed values
+    var total: Double {
+        let tipPercentage = Double(self.tipPercentages[self.tipPercentage])
+        let orderAmount = Double(self.orderSubtotal) ?? 0
+        
+        return orderAmount + (orderAmount * (tipPercentage/100))
+    }
+    
+    var totalByPerson: Double {
+        let peopleCount = Double(self.numberOfPeople + 2)
+        
+        return self.total / peopleCount
+    }
+    
+    // MARK: - Body
     var body: some View {
-        Text("Olá")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Valor", text: $orderSubtotal)
+                    .keyboardType(.decimalPad)
+                }
+                
+                Section {
+                    Picker("Número de pessoas",
+                           selection: self.$numberOfPeople) {
+                            ForEach(2..<50) {
+                                Text("\($0) people")
+                            }
+                    }
+                }
+                
+                Section(header: Text("Escolha a pocentagem da taxa de serviço:")) {
+                    Picker("Taxa de serviço",
+                           selection: self.$tipPercentage) {
+                            ForEach(0..<self.tipPercentages.count) {
+                                Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+
+                }
+                
+                Section(header: Text("Total:")) {
+                    Text("R$\(self.total, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Total por pessoa:")) {
+                    Text("R$\(self.totalByPerson, specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("WeSplit")
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
